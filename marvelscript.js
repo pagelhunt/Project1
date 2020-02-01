ad0c5da05d2b4896139fa69ddaefccb1d0482eb5
pagelhunt <pagelhunt@gmail.com>
2020-02-01 15:56:19 -0700

    added all attribute for marvel api and all elements have classses

--- marvelscript.js
+++ marvelscript.js
@@ -1,5 +1,6 @@
 
 let inputElement = document.getElementById('#input1');
+var loreHolder = $("#loreHolder");
 
 var superHeroArray = [];
 let queryURL = "https://akabab.github.io/superhero-api/api/all.json";
@@ -14,35 +15,41 @@ $.ajax({
 });
 
 $("#searchButton").on("click", function () {
-    var userInput = $("#searchText").val().trim();
+event.preventDefault();
+loreHolder.empty();
+    
+var userInput = $("#searchText").val().trim();
 
 var matchedSuperHero = superHeroArray.filter(superHero => {
 
 if(superHero.name.toLowerCase() === userInput.toLowerCase())
-  {
-    var superHeroObj = new Object();
-    superHeroObj["name"] = superHero.name;
-    superHeroObj["powerstats"] = superHero.powerstats;
-    superHeroObj["appearance"] = superHero.appearance;
-    superHeroObj["biography"] = superHero.biography;
-    superHeroObj["work"] = superHero.work;
-    superHeroObj["connections"] = superHero.connections;
-    return superHeroObj;
-  }
-})
-
-if(matchedSuperHero[0] == undefined)
-{
+        {
+            var superHeroObj = new Object();
+            superHeroObj["name"] = superHero.name;
+            superHeroObj["powerstats"] = superHero.powerstats;
+            superHeroObj["appearance"] = superHero.appearance;
+            superHeroObj["biography"] = superHero.biography;
+            superHeroObj["work"] = superHero.work;
+            superHeroObj["connections"] = superHero.connections;
+            return superHeroObj;
+        }
+    })
+  if(matchedSuperHero[0] == undefined)
+    {
     var errorMsg = $("<p>");
     errorMsg.text("Only A-Bomb is here! Try again!")
     $("#loreHolder").append(errorMsg);
 }else{
 
 var heroNameDiv = $("<div>");
+heroNameDiv.addClass("heroNameDiv heroName");
 var heroName = $("<h1>");
+heroName.addClass("heroName");
 heroName.text(matchedSuperHero[0].name);
 heroNameDiv.append(heroName);
-$("#loreHolder").append(heroNameDiv);
+loreHolder.append(heroNameDiv);
+
+
 //Power stats section --------------------------------------------------------------------------------
 var powerStatsDiv = $("<div>");
 var powerStatsDurability = $("<p>")
@@ -70,20 +77,101 @@ powerStatsDiv.append(powerStatsSpeed);
 powerStatsDiv.append(powerStatsDurability);
 powerStatsDiv.append(powerStatsPower);
 powerStatsDiv.append(powerStatsCombat);
-$("#loreHolder").append(powerStatsDiv);
+loreHolder.append(powerStatsDiv);
+//--------------------------------------------------------------------------------------------------
+
+//Appearance----------------------------------------------------------------------------------------
+var appearanceDiv = $("<div>");
+var gender = $("<p>");
+var race = $("<p>");
+var height = $("<p>");
+var weight = $("<p>");
+var eyeColor = $("<p>");
+var hairColor = $("<p>");
+
+appearanceDiv.addClass("appearance appearanceDiv");
+gender.addClass("appearance");
+race.addClass("appearance");
+height.addClass("appearance");
+weight.addClass("appearance");
+eyeColor.addClass("appearance");
+hairColor.addClass("appearance");
+
+gender.text("Gender: " + matchedSuperHero[0].appearance.gender);
+race.text("Race: " + matchedSuperHero[0].appearance.race);
+height.text("Height: " + matchedSuperHero[0].appearance.height[0]);
+weight.text("Weight :" + matchedSuperHero[0].appearance.weight[0]);
+eyeColor.text("Eye color: " + matchedSuperHero[0].appearance.eyeColor);
+hairColor.text("Hair Color: " + matchedSuperHero[0].appearance.hairColor)
+
+appearanceDiv.append(gender);
+appearanceDiv.append(race);
+appearanceDiv.append(height);
+appearanceDiv.append(weight);
+appearanceDiv.append(eyeColor);
+appearanceDiv.append(hairColor);
+loreHolder.append(appearanceDiv);
+
+
+//--------------------------------------------------------------------------------------------------
+
+//Bio-----------------------------------------------------------------------------------------------
+var BioDiv = $("<div>");
+var fullName= $("<p>");
+var placeOfBirth = $("<p>");
+var alignment = $("<p>");
+
+BioDiv.addClass("bio bioDiv");
+fullName.addClass("bio");
+placeOfBirth.addClass("bio");
+alignment.addClass("bio");
+
+fullName.text("Full Name: " + matchedSuperHero[0].biography.fullName);
+placeOfBirth.text("Place of Birth: " + matchedSuperHero[0].biography.placeOfBirth);
+alignment.text("Alignment: " + matchedSuperHero[0].biography.alignment);
+
+BioDiv.append(fullName);
+BioDiv.append(placeOfBirth);
+BioDiv.append(alignment);
+loreHolder.append(BioDiv);
 //--------------------------------------------------------------------------------------------------
 
 
-var AppearanceDiv = $("<div>");
-var appearance = $("<p>")
-var BioDiv = $("<div>");
-var biography = $("<p>")
+//Work----------------------------------------------------------------------------------------------
 var workDiv = $("<div>");
-var work = $("<p>")
-var connectionsDiv = $("<div>");
-var connections = $("<p>")
+var Occupation = $("<p>");
 
+workDiv.addClass("work workDiv");
+Occupation.addClass("work");
+
+Occupation.text("Occupation: " + matchedSuperHero[0].work.Occupation);
+
+workDiv.append(Occupation);
+loreHolder.append(workDiv);
+
+//--------------------------------------------------------------------------------------------------
+
+
+//Connections---------------------------------------------------------------------------------------
+var connectionsDiv = $("<div>");
+var groupAffiliation = $("<p>");
+var relatives = $("<p>")
+
+connectionsDiv.addClass("connections connectionsDiv");
+groupAffiliation.addClass("connections");
+relatives.addClass("connections");
+
+groupAffiliation.text("Affiliations: " + matchedSuperHero[0].connections.groupAffiliation)
+relatives.text("Relatives: " + matchedSuperHero[0].connections.relatives);
+
+connectionsDiv.append(groupAffiliation);
+connectionsDiv.append(relatives);
+loreHolder.append(connectionsDiv);
+//--------------------------------------------------------------------------------------------------
 }
+
+
+
 });
 
 
