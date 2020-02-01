955f8d80eecaf4bd9ea552a52e4ed5327bff61c0
Indigo Violet <indigo.violet.x@gmail.com>
2020-02-01 14:03:42 -0700

    Renamed script.js to homepagescript.js

--- script.js
+++
@@ -1,7 +0,0 @@
-$(".marEnterBttn").on("click",function(){
-    location.href="marvelpage.html";
-    });
-
-    $(".swEnterBttn").on("click",function(){
-        location.href="starwarspage.html";
-        });
