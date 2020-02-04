b949f3f2dccbe5d3f9e519bd80b5d0f5367fd465
Indigo Violet <indigo.violet.x@gmail.com>
2020-02-04 01:34:37 -0700

    CSS changes, added Chewbacca sound to 1st button, Darth Vader theme added as first theme

--- swMusicNSounds.js
+++ swMusicNSounds.js
@@ -4,16 +4,25 @@
         var captainPlanet = $(".captain-planet");
 
         // Gets Link for Theme Song
+
+        var darthVaderAudio = document.createElement("audio");
+        darthVaderAudio.setAttribute("src", "assets/music-sounds/DarthVader.mp3");
+
         var audioElement = document.createElement("audio");
-        audioElement.setAttribute("src", "assets/captainplanet24.mp3");
+        audioElement.setAttribute("src", "assets/music-sounds/ChewbaccaNoise.mp3");
 
         // Theme Button
         $(".theme-button").on("click", function() {
-          audioElement.play();
+          darthVaderAudio.play();
         });
         $(".pause-button").on("click", function() {
-          audioElement.pause();
+          darthVaderAudio.pause();
         });
-      });
+
+        $(".redbttn").on("click",function(){
+          audioElement.play();
+          });
+        });
+    
 
      
